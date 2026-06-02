f765dd9b6d59fe684fe658576240c8f8b5933367
DanielBata <batahernandez12345@gmail.com>
2026-06-02 15:48:23 -0500

    Fix/Data-Layer

--- Project/Data/Interfaces/IRepository/IDataGeneric.cs
+++ Project/Data/Interfaces/IRepository/IDataGeneric.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Data.Repository
+namespace Data.Interfaces.IRepository
 {
-    internal class ProductRepository
+    internal class IDataGeneric
     {
     }
 }
