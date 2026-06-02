1ac9dbd7de0d1e5a66e5bcf78f5124f6de756ab4
DanielBata <batahernandez12345@gmail.com>
2026-06-02 17:09:16 -0500

    Fix/Business-layer

--- Project/Business/obj/Debug/net9.0/Business.AssemblyInfo.cs
+++ Project/Business/obj/Debug/net9.0/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+257bd531f83f1833c7bf71b1aedd5306940c5504")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f765dd9b6d59fe684fe658576240c8f8b5933367")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Project/Entity/obj/Debug/net9.0/Entity.AssemblyInfo.cs
+++ Project/Entity/obj/Debug/net9.0/Entity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d3cc28d693bb3c2d51cd88a576fa98f25d4e510")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f765dd9b6d59fe684fe658576240c8f8b5933367")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
