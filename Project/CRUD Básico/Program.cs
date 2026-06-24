b29f3cce4e568a483530791a3ba6971a7217d6ac
DanielBata <batahernandez12345@gmail.com>
2026-06-24 17:07:50 -0500

    feat: configure api dependencies

--- Project/CRUD Básico/Program.cs
+++ Project/CRUD Básico/Program.cs
@@ -1,10 +1,39 @@
+using Business.Interfaces.Implements.Products;
+using Business.Mapping;
+using Business.Services.Products;
+using Data.Interfaces.Implements.Products;
+using Data.Interfaces.IRepository;
+using Data.Repository;
+using Data.Service.Products;
+using Entity.Infrastructure.Context;
+using Entity.Model;
+using Mapster;
+using MapsterMapper;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+// OpenAPI
 builder.Services.AddOpenApi();
+
+// Database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Mapster
+var mapsterConfig = MapsterConfig.Register();
+builder.Services.AddSingleton(mapsterConfig);
+builder.Services.AddScoped<IMapper, ServiceMapper>();
+
+// Data layer
+builder.Services.AddScoped<IDataGeneric<Product>, DataGeneric<Product>>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+// Business layer
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
--- Project/CRUD Básico/obj/Debug/net9.0/CRUD Básico.AssemblyInfo.cs
+++ Project/CRUD Básico/obj/Debug/net9.0/CRUD Básico.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD Básico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9d8ab814a07201f07b203fbf9d81f271aa6eede")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b8e1215845d970e47fc8795358a6a3d6fba0ae5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD Básico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD Básico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
